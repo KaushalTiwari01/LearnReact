ef3ec959f7468191b43f53761b2f16ac9b89a1ee
Kaushal Tiwari <tiwarikaushal868@gmail.com>
2025-06-28 10:41:54 +0530

    Initial React Project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <h1>this is the first heading.</h1>
+          <h2>this is the second heading.</h2>
+            <h3>this is the third heading.</h3>
+              <h4>this is the fourth heading.</h4>
+                <h5>this is the fifth heading.</h5>
+      </div>
+    </div>
+  );
+}
+
+export default App;
